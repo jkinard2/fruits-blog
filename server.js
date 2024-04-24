482023618cf6bd0d2b8c494ea9a4eff1029a855f
Rome Bell <romebell5@gmail.com>
2024-04-23 20:28:48 -0700

    feat: add show route

--- server.js
+++ server.js
@@ -6,9 +6,21 @@ const PORT = process.env.PORT || 3000;
 const fruits = ['Apples', 'Kiwi', 'Blueberries', 'Mangos', 'Strawberries'];
 
 // ------------ ROUTES ---------------
+// ******* INDEX ROUTE **********
 app.get('/fruits', (req, res) => {
     // send array as a response
     res.send(fruits);
+});
+
+// ******* SHOW ROUTE **********
+app.get('/fruits/:indexOfFruitsArray', (req, res) => {
+    let idx = parseInt(req.params.indexOfFruitsArray);
+    if (idx >= fruits.length) {
+        res.send('There is no fruit at that index.'); // one solution
+        // res.send(fruits);
+    } else {
+        res.send(fruits[idx]);
+    }
 });
 
 // ----------- LISTEN FOR SERVER ----------
